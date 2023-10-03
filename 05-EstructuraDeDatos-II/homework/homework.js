f815f001d7bc48ac5aa28f74dbe3546742dd6c9f
J JoaquínCastillo <117773656+joaco-0308@users.noreply.github.com>
2023-10-03 12:00:58 -0300

    actualizacion

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -1,18 +1,75 @@
-'use strict';
+"use strict";
 
 /* EJERCICIO 1
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  - remove: elimina el último nodo de la lista y retorna su valor 
+  (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: 
+  el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado;
+  en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
   EJEMPLO 
   search(3) busca un nodo cuyo valor sea 3;
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
+function LinkedList() {
+  this.head = null;
+}
 
-function Node(value) {}
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+LinkedList.prototype.add = function (valor) {
+  let nodo = new Node(valor);
+  let current = this.head;
+
+  if (!current) {
+    this.head = nodo;
+    return nodo;
+  }
+
+  while (current.next) {
+    current = current.next;
+  }
+
+  current.next = nodo;
+  return nodo;
+};
+LinkedList.prototype.remove = function () {
+  let current = this.head;
+  if (!this.head) return null;
+
+  if (!current.next) {
+    let sinNodo = current.value;
+    this.head = null;
+    return sinNodo;
+  }
+
+  while (current.next.next) {
+    current = current.next;
+  }
+
+  let eliminarNodo = current.next.value;
+  current.next = null;
+  return eliminarNodo;
+};
+
+LinkedList.prototype.search = function (arg) {
+  let current = this.head;
+
+  while(current){
+    if(typeof arg === "function"){
+      if(arg(current.value)) return current.value;
+    }else{
+      if(current.value === arg) return arg;
+    }
+    current = current.next;
+  }
+  return null;
+};
 
 /* EJERCICIO 2
 Implementar la clase HashTable.
@@ -21,19 +78,51 @@ Para este ejercicio, la tabla debe tener 35 buckets (numBuckets = 35). (Luego de
 
 La clase debe tener los siguientes métodos:
   - hash: función hasheadora que determina en qué bucket se almacenará un dato. Recibe un input alfabético, suma el código numérico de cada caracter del input (investigar el método charCodeAt de los strings) y calcula el módulo de ese número total por la cantidad de buckets; de esta manera determina la posición de la tabla en la que se almacenará el dato.
-  - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash, y almacena todo el conjunto en el bucket correcto.
+  - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash,
+   y almacena todo el conjunto en el bucket correcto.
   - get: recibe una clave por parámetro, y busca el valor que le corresponde en el bucket correcto de la tabla.
   - hasKey: recibe una clave por parámetro y consulta si ya hay algo almacenado en la tabla con esa clave (retorna un booleano).
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function HashTable() {
+  this.table = [];
+  this.numBuckets = 35
+}
+
+HashTable.prototype.hash = function(key){
+  let hash = 0;
+  for(let i = 0; i < key.length; i++){
+    hash += KeyboardEvent.charCodeAt(key)
+  }
+  return hash % this.numBuckets;
+}
+
+HashTable.prototype.set = function(key, value){
+  if(typeof key !== "string")throw typeof("Keys must by strings")
+  let index = this.hash(key);
+
+  if(!this.table[index]){
+    this.table[index] = {};
+  }
+  this.table[index][key] = value;
+}
+
+HashTable.prototype.get = function(key){
+  let index = this.hash(key);
+  return this.table[index][key];
+}
+
+HashTable.prototype.hasKey = function(key){
+  let index = this.hash(key);
+  return !!this.table[index][key]; //!! sirve para convertir en boolenano.
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   Node,
-   LinkedList,
-   HashTable,
+  Node,
+  LinkedList,
+  HashTable,
 };
--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,19 +1,101 @@
-'use strict';
+"use strict";
 
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
   - size: retorna la cantidad total de nodos del árbol
   - insert: agrega un nodo en el lugar correspondiente
   - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
+  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes,
+   según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro,
+   hará el recorrido "in-order" por defecto.
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value < this.value) {
+    if (this.left) {
+      return this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+      return value;
+    }
+  } else {
+    if (this.right) {
+      return this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+      return value;
+    }
+  }
+};
+
+BinarySearchTree.prototype.size = function () {
+   if(!this.left && !this.right) return 1;
+
+   if(this.left && !this.right) return this.left.size() +1;
+
+   if(!this.left && this.right) return this.right.size() +1;
+
+   else return (this.left.size() + this.right.size() +1);
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+if(this.value === value) return true;
+
+if(value < this.value){
+   if(this.left && this.left.value === value){
+      return true;
+   }else if(this.left){
+      return this.left.contains(value);
+   }
+}else{
+   if(this.right && this.right.value === value){
+      return true;
+   }else if(this.right){
+      return this.right.contains(value);
+   }
+}
+return false;
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, arg) {
+   switch(arg){
+      case "post-order":
+          if(this.left) this.left.depthFirstForEach(cb, arg);
+          if(this.right) this.right.depthFirstForEach(cb, arg);
+          cb(this.value);
+          break;
+      case "pre-order":
+         cb(this.value);
+         if(this.left) this.left.depthFirstForEach(cb, arg);
+         if(this.right) this.right.depthFirstForEach(cb, arg);
+         break;
+      default:
+         if(this.left) this.left.depthFirstForEach(cb, arg);
+         cb(this.value);
+         if(this.right) this.right.depthFirstForEach(cb, arg);
+         break;
+   }
+};
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array = []) {
+if(this.left != null) array.push(this.left);
+
+if(this.right != null) array.push(this.right);
+
+cb(this.value);
+
+if(array.length > 0) return array.shift().breadthFirstForEach(cb, array);
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   BinarySearchTree,
+  BinarySearchTree,
 };
