ae8dcbeee0a8b3ea4d5ea50c13231ba12ae90f03
J JoaquínCastillo <117773656+joaco-0308@users.noreply.github.com>
2023-10-04 20:16:19 -0300

    actualizar

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,7 @@
-'use strict'
+"use strict";
+
+const { log } = require("console");
+
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,35 +9,76 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let factores = [1];
+  let x = 2;
 
+  while (num > 1) {
+    if (num % x === 0) {
+      factores.push(x);
+      num = num / x;
+    } else {
+      x++;
+    }
+  }
+  return factores;
 }
+
+//console.log(factorear(4));
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let index = array[i];
+        array[i] = array[j];
+        array[j] = index;
+      }
+    }
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    let index = array[i];
+    while (j >= 0 && array[j] > index) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = index;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let index = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[index]) {
+        index = j;
+      }
+    }
+    if (index !== i) {
+      let aux = array[i];
+      array[i] = array[index];
+      array[index] = aux;
+    }
+  }
+  return array;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
